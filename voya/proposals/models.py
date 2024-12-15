a57b80a3d29ce463d6c86c2d568ef3a8351fcb43
Ivelina Karagyulieva <ivelina.karagulieva@gmail.com>
2024-12-15 21:52:04 +0100

    License set up

--- voya/proposals/models.py
+++ voya/proposals/models.py
@@ -1,5 +1,23 @@
+"""
+Copyright (C) 2025 DROMO SA
+
+This file is part of VOYA.
+
+VOYA is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+[Project Name] is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
@@ -9,7 +27,6 @@ from voya.requests import choices
 from voya.requests.models import TripRequests
 from voya.services.models import Currency
 
-# Create your models here.
 
 User = get_user_model()
 
