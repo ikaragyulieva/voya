a37029597a355680189677f71c63e45dcee5bc00
Ivelina Karagyulieva <ivelina.karagulieva@gmail.com>
2025-02-11 12:51:55 +0100

    allowed hosts setting

--- voya/settings.py
+++ voya/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 PROJECT_APPS = [
