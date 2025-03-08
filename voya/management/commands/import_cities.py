457814a8a6c78a65e11450e2e3338080391eba4a
Ivelina Karagyulieva <ivelina.karagulieva@gmail.com>
2025-03-08 13:36:58 +0100

    new cities added to command in order to update location model cities

--- voya/management/commands/import_cities.py
+++ voya/management/commands/import_cities.py
@@ -111,6 +111,17 @@ class Command(BaseCommand):
             {"city": "Zagreb", "country": "HR"},
             {"city": "Zaragoza", "country": "ES"},
             {"city": "Zurich", "country": "CH"},
+            {"city": "Grenoble", "country": "FR"},
+            {"city": "Jerez de la Frontera", "country": "ES"},
+            {"city": "Chefchaouen", "country": "MAR"},
+            {"city": "FEZ", "country": "MAR"},
+            {"city": "Rabat", "country": "MAR"},
+            {"city": "Reims", "country": "FR"},
+            {"city": "Tanger", "country": "MAR"},
+            {"city": "Tarragona", "country": "ES"},
+            {"city": "Dudley", "country": "GB"},
+            {"city": "Leicester", "country": "GB"},
+            {"city": "Generic", "country": "ES"},
         ]
 
         existing_cities = set(Location.objects.values_list("city", flat=True))
