186de018d9805e5f2dfd6b417c45b26756f41403
Ivelina Karagyulieva <ivelina.karagulieva@gmail.com>
2026-04-08 22:54:07 +0200

    fix issues with container reloading in production, inside dokploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 DB_HOST="${DB_HOST}"
 DB_PORT="${DB_PORT}"
+DB_USER="${DB_USER}"
+DB_NAME="${DB_NAME}"
 
 echo "Waiting for DNS: $DB_HOST ..."
 for i in $(seq 1 60); do
@@ -16,13 +18,13 @@ getent hosts "$DB_HOST" >/dev/null 2>&1 || { echo "DNS for $DB_HOST failed"; exi
 
 echo "Waiting for Postgres on $DB_HOST:$DB_PORT ..."
 for i in $(seq 1 60); do
-  if pg_isready -h "$DB_HOST" -p "$DB_PORT" >/dev/null 2>&1; then
+  if pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" >/dev/null 2>&1; then
     break
   fi
   echo "  [$i] DB not ready yet..."
   sleep 2
 done
-pg_isready -h "$DB_HOST" -p "$DB_PORT" >/dev/null 2>&1 || { echo "DB not reachable"; exit 1; }
+pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" >/dev/null 2>&1 || { echo "DB not reachable"; exit 1; }
 
 echo "Running migrations..."
 python manage.py migrate --noinput
